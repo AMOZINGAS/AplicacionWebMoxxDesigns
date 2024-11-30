7300a1de7d9ca79e43738ed3c4a413e91e37ecc7
777Styx <jullianpuerta@gmail.com>
2024-11-29 21:44:43 -0800

    encrypting my pana redy

--- MoxxDesignsNegocios/src/main/java/itson/mx/moxxdesignsnegocios/utils/Encrypt.java
+++ MoxxDesignsNegocios/src/main/java/itson/mx/moxxdesignsnegocios/utils/Encrypt.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package itson.mx.moxxdesignsnegocios.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  *
  * @author olive
  */
 public class Encrypt {
-    
+
+    public static String encrypt(String input) {
+        try {
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashBytes = digest.digest(input.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
