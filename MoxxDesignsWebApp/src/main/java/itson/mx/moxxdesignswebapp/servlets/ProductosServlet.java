5a5bb9dad1d28bdbb4bc5f4785a959cbd5af0d37
777Styx <jullianpuerta@gmail.com>
2024-11-30 02:09:48 -0800

    dejen dormir

--- MoxxDesignsWebApp/src/main/java/itson/mx/moxxdesignswebapp/servlets/ProductosServlet.java
+++ MoxxDesignsWebApp/src/main/java/itson/mx/moxxdesignswebapp/servlets/ProductosServlet.java
@@ -33,14 +33,14 @@ public class ProductosServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Obtén el parámetro "id" de la consulta
+
         String idParam = request.getParameter("id");
 
         if (idParam != null) {
-            // Llama al método para obtener el producto por ID
+
             GETObtenerProductoPorId(request, response);
         } else {
-            // Si no se proporciona un "id", devuelve todos los productos
+
             GETObtenerTodosLosProductos(request, response);
         }
     }
