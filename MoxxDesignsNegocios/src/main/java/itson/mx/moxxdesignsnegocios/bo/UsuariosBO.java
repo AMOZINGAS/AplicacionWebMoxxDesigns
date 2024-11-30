fc33610c1f348b70df45e7e3df5cf2114a7e773f
777Styx <jullianpuerta@gmail.com>
2024-11-29 22:11:25 -0800

    encriptacion en crear usuario
    ...

--- MoxxDesignsNegocios/src/main/java/itson/mx/moxxdesignsnegocios/bo/UsuariosBO.java
+++ MoxxDesignsNegocios/src/main/java/itson/mx/moxxdesignsnegocios/bo/UsuariosBO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package itson.mx.moxxdesignsnegocios.bo;
 
 import itson.mx.moxxdesignsdominio.conexion.Conexion;
@@ -12,6 +8,7 @@ import itson.mx.moxxdesignsexcepciones.NegociosException;
 import itson.mx.moxxdesignsexcepciones.PersistenciaException;
 import itson.mx.moxxdesignsnegocios.utils.Convertor;
 import itson.mx.moxxdesignsnegocios.interfaces.IUsuariosBO;
+import itson.mx.moxxdesignsnegocios.utils.Encrypt;
 import itson.mx.moxxdesignspersistencia.daos.CarritoComprasDAO;
 import itson.mx.moxxdesignspersistencia.daos.UsuariosDAO;
 import itson.mx.moxxdesignspersistencia.interfaces.ICarritoComprasDAO;
@@ -22,43 +19,47 @@ import itson.mx.moxxdesignspersistencia.interfaces.IUsuariosDAO;
  * @author olive
  */
 public class UsuariosBO implements IUsuariosBO {
-    
-    private IUsuariosDAO usuariosDAO ;
-    private ICarritoComprasDAO carritoDAO ;
-    
+
+    private IUsuariosDAO usuariosDAO;
+    private ICarritoComprasDAO carritoDAO;
+
     public UsuariosBO() {
-        this.usuariosDAO = new UsuariosDAO(new Conexion()) ;
-        this.carritoDAO = new CarritoComprasDAO(new Conexion()) ;
+        this.usuariosDAO = new UsuariosDAO(new Conexion());
+        this.carritoDAO = new CarritoComprasDAO(new Conexion());
     }
 
     @Override
     public UsuarioDTO crearUsuario(UsuarioDTO usuario) throws NegociosException {
         try {
-            Usuario usuarioCreado = usuariosDAO.crearUsuario(Convertor.usuarioDtoAEntity(usuario)) ;
+
+            String contrasenaEncriptada = Encrypt.encrypt(usuario.getPassword());
+            usuario.setPassword(contrasenaEncriptada);
+
+            Usuario usuarioCreado = usuariosDAO.crearUsuario(Convertor.usuarioDtoAEntity(usuario));
             carritoDAO.crearCarritoDeCompras(new CarritoCompras(usuarioCreado));
-            return Convertor.usuarioEntityADto(usuarioCreado) ;
+            return Convertor.usuarioEntityADto(usuarioCreado);
         } catch (PersistenciaException e) {
-            throw new NegociosException(e.getMessage()) ;
+            throw new NegociosException(e.getMessage());
         }
     }
 
     @Override
     public boolean login(String email, String password) throws NegociosException {
         try {
-            return usuariosDAO.login(email, password) ;
+            return usuariosDAO.login(email, password);
         } catch (PersistenciaException e) {
-            throw new NegociosException(e.getMessage()) ;
+            throw new NegociosException(e.getMessage());
         }
     }
 
     @Override
     public UsuarioDTO obtenerUsuarioPorEmail(String email) throws NegociosException {
         try {
-            Usuario usuarioObtenido = usuariosDAO.obtenerUsuarioPorEmail(email) ;
-            return Convertor.usuarioEntityADto(usuarioObtenido) ;
+            Usuario usuarioObtenido = usuariosDAO.obtenerUsuarioPorEmail(email);
+            return Convertor.usuarioEntityADto(usuarioObtenido);
         } catch (PersistenciaException e) {
-            throw new NegociosException(e.getMessage()) ;
+            throw new NegociosException(e.getMessage());
         }
     }
-    
+
 }
